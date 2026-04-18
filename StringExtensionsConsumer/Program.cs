6ce9d386432ac2ec7cd3cdea981abf8db12ab33a
Jerome Quayson <236247694+3th34-Byte@users.noreply.github.com>
2026-04-18 16:41:16 -0700

    Add full solution files

---
+++ StringExtensionsConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringExtensionsLib;
+
+namespace StringExtensionsConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string:");
+            var input = Console.ReadLine();
+
+            bool result = input.StartsWithUppercase();
+
+            Console.WriteLine(result
+                ? "Starts with uppercase"
+                : "Does NOT start with uppercase");
+
+        }
+    }
+}
---
+++ StringExtensionsLib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUppercase(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
