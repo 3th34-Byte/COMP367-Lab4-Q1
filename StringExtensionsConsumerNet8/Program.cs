135325ea239278bb32f5a4be42561aa320634f78
Jerome Quayson <236247694+3th34-Byte@users.noreply.github.com>
2026-04-18 18:15:51 -0700

    Add .NET 8 projects and fix pipeline

---
+++ StringExtensionsConsumerNet8/Program.cs
@@ -0,0 +1,10 @@
+﻿using StringExtensionsLib;
+
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine() ?? "";
+
+bool result = input.StartsWithUppercase();
+
+Console.WriteLine(result
+    ? "Starts with uppercase"
+    : "Does NOT start with uppercase");
